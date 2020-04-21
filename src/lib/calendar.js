1e6d6d9ab97e74300a399f21c9042ce7e122fb00
Angelo <angelo@rcstart.it>
2020-04-21 09:12:30 +0200

    New Calendar

--- src/lib/calendar.js
+++ src/lib/calendar.js
@@ -1,38 +1,38 @@
 const calendar = [{
-    index: 1,
-    day_of_the_week: 'Lunedì',
-    waste_to_expose: 'Umido'
-  },
-  {
-    index: 2,
-    day_of_the_week: 'Martedì',
-    waste_to_expose: 'Indifferenziato'
-  },
-  {
-    index: 3,
-    day_of_the_week: 'Mercoledì',
-    waste_to_expose: 'Carta'
-  },
-  {
-    index: 4,
-    day_of_the_week: 'Giovedì',
-    waste_to_expose: 'Vetro'
-  },
-  {
-    index: 5,
-    day_of_the_week: 'Venerdì',
-    waste_to_expose: 'Plastica e Metalli'
-  },
-  {
-    index: 6,
-    day_of_the_week: 'Sabato',
-    waste_to_expose: 'Indifferenziato'
-  },
-  {
-    index: 0,
-    day_of_the_week: 'Domenica',
-    waste_to_expose: 'Giorno di riposo'
-  }
+  index: 1,
+  day_of_the_week: 'Lunedì',
+  waste_to_expose: 'Indifferenziato'
+},
+{
+  index: 2,
+  day_of_the_week: 'Martedì',
+  waste_to_expose: 'Umido'
+},
+{
+  index: 3,
+  day_of_the_week: 'Mercoledì',
+  waste_to_expose: 'Carta'
+},
+{
+  index: 4,
+  day_of_the_week: 'Giovedì',
+  waste_to_expose: 'Vetro'
+},
+{
+  index: 5,
+  day_of_the_week: 'Venerdì',
+  waste_to_expose: 'Plastica e Metalli'
+},
+{
+  index: 6,
+  day_of_the_week: 'Sabato',
+  waste_to_expose: 'Indifferenziato'
+},
+{
+  index: 0,
+  day_of_the_week: 'Domenica',
+  waste_to_expose: 'Giorno di riposo'
+}
 ];
 
 module.exports = calendar
