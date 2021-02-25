1361ce5e61a7e86e1621c2fde2542bec9d3aa67f
Angelo <angelo@rcstart.it>
2021-02-25 18:41:20 +0100

    calendar update

--- src/lib/calendar.js
+++ src/lib/calendar.js
@@ -82,6 +82,58 @@ const calendar = [
   {
     date: "2021-02-21",
     waste_to_expose: "",
+  },
+  {
+    date: "2021-02-22",
+    waste_to_expose: "Umido e Pannolini",
+  },
+  {
+    date: "2021-02-23",
+    waste_to_expose: "Indifferenziato e Pannolini",
+  },
+  {
+    date: "2021-02-24",
+    waste_to_expose: "Carta e Cartone",
+  },
+  {
+    date: "2021-02-25",
+    waste_to_expose: "Umido e Pannolini",
+  },
+  {
+    date: "2021-02-26",
+    waste_to_expose: "Plastica, Metalli e Pannolini",
+  },
+  {
+    date: "2021-02-27",
+    waste_to_expose: "Vetro",
+  },
+  {
+    date: "2021-02-28",
+    waste_to_expose: "",
+  },
+  {
+    date: "2021-03-01",
+    waste_to_expose: "Indifferenziato e Pannolini",
+  },
+  {
+    date: "2021-03-02",
+    waste_to_expose: "Carta e Cartone",
+  },
+  {
+    date: "2021-03-03",
+    waste_to_expose: "Umido e Pannolini",
+  },
+  {
+    date: "2021-03-04",
+    waste_to_expose: "Plastica, Metalli e Pannolini",
+  },
+  {
+    date: "2021-03-05",
+    waste_to_expose: "Vetro",
+  },
+  {
+    date: "2021-03-06",
+    waste_to_expose: "",
   }
 ];
 
--- src/scenes/show_calendar.js
+++ src/scenes/show_calendar.js
@@ -14,7 +14,7 @@ showCalendar.enter(ctx => {
       return `${moment(new Date(data.date)).format('DD/MM')} - ${data.waste_to_expose}\n`
     }).join("")
 
-    const lastUpdateAt = `_Ultimo aggiornamento: 15/02/2021_`
+    const lastUpdateAt = `_Ultimo aggiornamento: 25/02/2021_`
 
     const text =
       `🗓 *Calendario Settimanale*\n\n${textCalendar}\n${lastUpdateAt}`;
